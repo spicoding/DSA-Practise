b6c219ce6db4814ca6faaa96ced69a5272d4cec9
spicoding <shaneinduli@gmail.com>
2024-08-08 15:39:54 +0300

    2nd Commit

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -7,3 +7,19 @@ for i in range(n-1):
             my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
 
 print("Sorted array:", my_array)
+
+# 2ND APPLICATION (IMPROVED)
+
+my_array2 = [7, 3, 9, 12, 11]
+
+n = len(my_array2)
+for i in range(n - 1):
+    swapped = False
+    for j in range(n - i - 1):
+        if my_array2[j] > my_array2[j + 1]:
+            my_array2[j], my_array2[j + 1] = my_array2[j + 1], my_array2[j]
+            swapped = True
+    if not swapped:
+        break
+
+print("Sorted array:", my_array2)
